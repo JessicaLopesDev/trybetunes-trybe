7d46c28842628f4a2c98a6b6cf3e5911704d5bdc
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-02-04 14:16:10 -0300

    Requisito 1 corrigido

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Header extends Component {
   render() {
+    const { name } = this.props;
     return (
-      <header data-testid="header-component">header</header>
+      <header data-testid="header-component">
+        <h2 data-testid="header-user-name">{ name }</h2>
+        { name }
+      </header>
     );
   }
 }
+
+Header.propTypes = {
+  name: PropTypes.string.isRequired,
+};
