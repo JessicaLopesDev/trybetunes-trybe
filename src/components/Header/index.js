26acd9f30146fcdc8b028c877f67145fcf98b6d0
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-02-04 14:53:22 -0300

    Requisito 4 ok

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../../services/userAPI';
 import Loading from '../Loading';
 
@@ -36,6 +37,24 @@ export default class Header extends Component {
             </h2>
           )
         }
+        <Link
+          to="/search"
+          data-testid="link-to-search"
+        >
+          Pesquisar
+        </Link>
+        <Link
+          to="/favorites"
+          data-testid="link-to-favorites"
+        >
+          Favoritas
+        </Link>
+        <Link
+          to="/profile"
+          data-testid="link-to-profile"
+        >
+          Perfil
+        </Link>
       </header>
     );
   }
